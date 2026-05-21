8b9d25eaac16a1967ac6d555d2c401becaa04463
Sazafo <sazafo1809@gmail.com>
2026-05-20 20:10:28 -0600

    Semana 1
    Creacion de proyecto semana1 y 2

---
+++ TechShop_Santiago/src/test/java/TechShop/Santiago/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.Santiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
